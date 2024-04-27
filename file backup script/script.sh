db2a6286fc48fe4ac8a73ab32a654113439c4f27
iam-lavanya <151933485+iam-lavanya@users.noreply.github.com>
2024-04-27 10:59:36 +0530

    Update script.sh

--- file backup script/script.sh
+++ file backup script/script.sh
@@ -3,6 +3,6 @@
 backup_dir="/path/to/backup diectory" 
 source_dir="/path/to/source directory" 
  
-tar -czf "$backup_dir/backup_$(date+%Y%m%d_%H%M%S).tar.gz" "$source_dir" # 
+tar -czf "$backup_dir/backup_$(date "+%Y-%m-%d %H:%M:%S").tar.gz)" "$source_dir" 
 
  
